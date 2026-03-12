7a7ccf66b546b97aa8e721902b4377521cbc6ebb
rajivhere <rajivhere@gmail.com>
2026-03-12 15:59:19 -0400

    initial-commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+interface ButtonProps {
+  children: React.ReactNode
+  onClick?: () => void
+}
+
+export function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  )
+}
